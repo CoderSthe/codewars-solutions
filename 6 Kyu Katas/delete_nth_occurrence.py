def2bf3caadcc3cf6e0cb6f09867bddc151e8d9f
Coder Sthe <shotleftdriver@gmail.com>
2022-06-29 00:25:23 +0200

    Additional katas added

---
+++ 6 Kyu Katas/delete_nth_occurrence.py
@@ -0,0 +1,14 @@
+def delete_nth(order,max_e):
+    """Given a list and a number, create a new list that contains each number of
+    [list] at most n times, without reordering.
+    EG. If the input number is 2, and the input list is [1,2,3,1,2,1,2,3],
+    you take [1,2,3,1,2], drop the next [1,2] since this would lead to 
+    1 and 2 being in the result 3 times, and then take 3, which
+    leads to [1,2,3,1,2,3]"""
+    
+    new_list = []
+    for num in order:
+        if new_list.count(num) < max_e:
+            new_list.append(num)
+            
+    return new_list
---
+++ 6 Kyu Katas/replace_with_alphabet.py
@@ -0,0 +1,24 @@
+def alphabet_position(text):
+    """This function replaces every letter in a given string
+    with its position in the alphabet.
+    Any character that is not an alphabet is ignored and not returned."""
+    
+    alphabet = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7,
+               'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14,
+               'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21,
+               'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
+
+    
+    text = text.lower().replace(" ", "")
+    for char in text:
+        if char not in alphabet:
+            text = text.replace(char, "")
+        else:
+            text = text.replace(char, str(alphabet[char]) + " ")
+
+        
+    return text
+
+
+print(alphabet_position("The sunset sets at twelve o' clock."))
+print(alphabet_position("The narwhal bacons at midnight."))
