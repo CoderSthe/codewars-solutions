a2b0f377db3626803db9f2e604181b77821b2877
Coder Sthe <shotleftdriver@gmail.com>
2022-06-29 09:11:52 +0200

    Updated replace_with_alphabet.py to remove whitespace

--- 6 Kyu Katas/replace_with_alphabet.py
+++ 6 Kyu Katas/replace_with_alphabet.py
@@ -2,7 +2,7 @@ def alphabet_position(text):
     """This function replaces every letter in a given string
     with its position in the alphabet.
     Any character that is not an alphabet is ignored and not returned."""
-    
+
     alphabet = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7,
                'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14,
                'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21,
@@ -17,7 +17,7 @@ def alphabet_position(text):
             text = text.replace(char, str(alphabet[char]) + " ")
 
         
-    return text
+    return text.rstrip()
 
 
 print(alphabet_position("The sunset sets at twelve o' clock."))
