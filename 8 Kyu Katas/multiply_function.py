de47ae135ff47477f6f6eb7ad184a5ca221ac9ac
Coder Sthe <shotleftdriver@gmail.com>
2022-07-03 17:21:19 +0200

    Modified multiply_function.py solution

--- 8 Kyu Katas/multiply_function.py
+++ 8 Kyu Katas/multiply_function.py
@@ -1,3 +1,7 @@
 def multiply(x, y):
     """This function multiplies two numbers and returns the result."""
     return x * y
+
+print(multiply(5,6))
+print(multiply(20.0,4.0))
+print(multiply(32.3,3))
