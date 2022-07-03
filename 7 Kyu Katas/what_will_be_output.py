fc471f2cdcb0781dab3ccd6f9a1080513cc1b1bc
Coder Sthe <shotleftdriver@gmail.com>
2022-07-03 17:23:58 +0200

    Added new 8 Kyu Katas

--- 7 Kyu Katas/what_will_be_output.py
+++
@@ -1,9 +0,0 @@
-# i = 1
-# while True:
-#     if i%0O7 == 0:
-#         break
-#     print(i)
-#     i += 1
-
-# i = 1
-# print(i % 0O7)
---
+++ 8 Kyu Katas/return_negative.py
@@ -0,0 +1,9 @@
+def make_negative( number ):
+    if number <= 0:
+        return number
+    else:
+        return -(number)
+
+print(make_negative(42))
+print(make_negative(-9))
+print(make_negative(0))
