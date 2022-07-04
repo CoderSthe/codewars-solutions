434dc5678e26912495ce95edd24ab043e26aa1be
Coder Sthe <shotleftdriver@gmail.com>
2022-07-04 11:11:46 +0200

    Added 8 Kyu Kata

---
+++ 8 Kyu Katas/abbreviate_two_word_name.py
@@ -0,0 +1,8 @@
+def abbrev_name(name):
+    return f"{name.split()[0][0]}.{name.split()[1][0]}".upper()
+
+print(abbrev_name("Sam Harris"))
+print(abbrev_name("patrick feenan"))
+print(abbrev_name("sithembiso Mdhluli"))
+print(abbrev_name("Evan C"))
+print(abbrev_name("P Simelane"))
